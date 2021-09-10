2d762d8d76375c932cb3ad83f268735336a0fe2b
VtitiV <60551609+VtitiV@users.noreply.github.com>
2021-09-10 10:07:35 +0200

    3em

---
+++ Resolve/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from import_export.admin import ImportExportModelAdmin
+
+from Resolve.models.personne import Personne
+
+
+@admin.register(Personne)
+class PersonneAdmin(ImportExportModelAdmin):
+    pass
