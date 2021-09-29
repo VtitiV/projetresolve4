5cf0d78abb8bf673c7df3facdfd4727c03a09e8e
VtitiV <60551609+VtitiV@users.noreply.github.com>
2021-09-29 20:29:34 +0200

    2

--- Resolve/urls.py
+++ Resolve/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from .views.incident import connexionhm
 from .views import home, personne
 from .views import incident
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path("personne/", personne.personne_list2, name="personne"),
     # vue incident/demande
     path("incidents/", incident.incidents_list, name="incident"),
+    # vue incidenthm
+    path("connexionhm/", incident.connexionhm, name="incidenthm"),
 ]
--- Resolve/views/incident.py
+++ Resolve/views/incident.py
@@ -5,3 +5,7 @@ def incidents_list(request):
     selected = "incident"
     return render(request, "resolve/incident/Incidents.html", locals())
 
+
+def connexionhm(request):
+    selected = "connexionHM"
+    return render(request, "resolve/incident/list_incident/connexionHM.html", locals())
